a67265011ab7477c6cb5304b269ce8d6f4c47efb
joaofsoares <joaofsoares@yahoo.com>
2022-10-01 13:50:51 +0100

    improv

--- learn/src/learn/functions/5kyu/product_of_fib.clj
+++ learn/src/learn/functions/5kyu/product_of_fib.clj
@@ -9,17 +9,12 @@
 
 (defn product-fib [prod]
   (loop [x 0]
-
-    (def fst
-      (nth fibonacci x))
-
-    (def snd
-      (nth fibonacci (inc x)))
-
-    (cond
-      (= (* fst snd) prod) [fst snd true]
-      (> (* fst snd) prod) [fst snd false]
-      :else (recur (inc x)))))
+    (let [fst (nth fibonacci x)
+          snd (nth fibonacci (inc x))]
+      (cond
+        (= (* fst snd) prod) [fst snd true]
+        (> (* fst snd) prod) [fst snd false]
+        :else (recur (inc x))))))
 
 (product-fib 714)
 (product-fib 800)
