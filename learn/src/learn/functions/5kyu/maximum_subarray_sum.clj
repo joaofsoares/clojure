a3c6642b654ab1176748bd90a85e55ba2022315c
joaofsoares <joaofsoares@yahoo.com>
2022-10-27 23:46:53 +0100

    maximum subarray sum

---
+++ learn/src/learn/functions/5kyu/maximum_subarray_sum.clj
@@ -0,0 +1,13 @@
+(ns learn.functions.5kyu.maximum-subarray-sum)
+
+(defn get-max
+  [xs current-sum max-sum]
+  (if (empty? xs)
+    max-sum
+    (let [fst (first xs)
+          cs (+ current-sum fst)]
+      (recur (rest xs) (max fst cs) (max max-sum cs)))))
+
+(defn max-sequence
+  [xs]
+  (get-max xs 0 0))
